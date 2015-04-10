be42eb362918bcf1e5149d6b5d7d76176a117a22
Viktor <victor.dzundza.dev@gmail.com>
2015-04-10 12:07:13 +0300

    added radio buttons for selecting answer

--- src/app/app-poll-directive.js
+++ src/app/app-poll-directive.js
@@ -1,27 +1,39 @@
- function PollDirective($timeout, PollFactory) {
-     return {
-         restrict: 'E',
-         templateUrl: 'poll-template.html',
-         scope: {
-             styleClass: '@styleClass'
-         },
-         link: function (scope, element, attrs) {
-             scope.loadedPoll = false;
-             PollFactory.get({
-                 id: attrs.pollId
-             });
+ (function () {
+     'use strict';
 
-             $timeout(function () {
-                 scope.loadedPoll = true;
-             }, 3000);
-         },
+     function PollDirective($timeout, PollFactory) {
+         return {
+             restrict: 'E',
+             templateUrl: 'poll-template.html',
+             scope: {
+                 styleClass: '@styleClass'
+             },
+             link: function (scope, element, attrs) {
+                 scope.loadedPoll = false;
+                 PollFactory.get({
+                     id: attrs.pollId
+                 }, scope.renderPoll, scope.renderLoadingError);
 
-         controller: function ($scope) {
-             $scope.submitAnswer = function () {
-                 alert('submit answer');
+                 $timeout(function () {
+                     scope.loadedPoll = true;
+                 }, 3000);
+             },
+
+             controller: function ($scope) {
+                 $scope.submitAnswer = function () {
+                     var pollViewModel = $scope.poll;
+                     console.log('submit answer[pollId, answerId]:', pollViewModel.pollId, pollViewModel.selectedAnswerId);
+                 };
+                 $scope.renderPoll = function (poll) {
+                     $scope.poll = poll;
+                 };
+
+                 $scope.renderLoadingError = function () {
+                     alert('error during loading poll');
+                 }
              }
-         }
-     };
- }
+         };
+     }
 
- angular.module('app')..directive('poll', ['$timeout', 'PollFactory', PollDirective]);
+     angular.module('app').directive('poll', ['$timeout', 'PollFactory', PollDirective]);
+ })();
